5702d1189f8d21961278ad153c01bbb44b3b41b6
Nerminko Omanic <omanic@harpaceas.it>
2025-05-29 16:39:57 +0200

    Change style to check vercel update

--- app/page.js
+++ app/page.js
@@ -3,7 +3,8 @@ export default function Home() {
     <main style={{ fontFamily: "sans-serif", padding: "2rem" }}>
       <h1>Unix Time API</h1>
       <p>
-        Get the current timestamp from <a href="/api/unixtime">/api/unixtime</a>
+        Get the current timestamp from{" "}
+        <a href="/api/unixtime">Unix Timestamp API</a>
       </p>
     </main>
   );
